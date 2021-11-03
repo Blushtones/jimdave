ae81bfe526976d5275ea53d8b6896fe28c862b9f
Plebster <plebster@hotmail.com>
2021-11-03 10:34:27 +0000

    Update ServerInfo.sh

--- ServerInfo.sh
+++ ServerInfo.sh
@@ -1,2 +1,2 @@
-# Tells us Linux Distro info, and version info for php, mysql, and apache. Also if phpmyadmin is installed
+# Tells us Linux Distro info, and version info for php, mysql, and apache. Also if phpmyadmin is installed.
 lsb_release -a; mysql -V; php -v; apache2 -v; dpkg --list | grep phpmyadmin
